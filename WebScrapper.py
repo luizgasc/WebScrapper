391e4520cac75d3fbdfd1012d95d6155a2a001f7
Luiz Gustavo <luizgasc@pop-os.localdomain>
2021-10-16 22:25:15 -0300

    Transforming table to a Dataframe

--- WebScrapper.py
+++ WebScrapper.py
@@ -1,12 +1,44 @@
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
+from urllib.error import URLError, HTTPError
+import pandas as pd
 
-url = 'https://www.nfl.com/stats/player-stats/'
+url = 'https://www.nfl.com/stats/team-stats/'
+headers = {}
+l = []
+header = []
 
-response = urlopen(url)
 
-html = response.read()
-soup = BeautifulSoup(html, 'html.parser')
+def decode_html(input):
+    return " ".join(input.split()).replace('> <', '><')
 
-print(soup.find('table'))
 
+try:
+    req = Request(url, headers=headers)
+    response = urlopen(req)
+    html = response.read()
+    html = html.decode('utf-8')
+    html = decode_html(html)
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find('table')
+    table_rows = table.find_all('tr')
+    table_headers = soup.findAll('th')
+    for th in table_headers:
+        tcolumn = th.text
+        header.append(tcolumn)
+
+    for tr in table_rows:
+        td = tr.findAll('td')
+        row = [tr.text for tr in td]
+        l.append(row)
+    df = pd.DataFrame(l)
+
+
+    df.columns = header
+    print(df)
+
+except HTTPError as e:
+    print(e.status, e.reason)
+
+except URLError as e:
+    print(e.reason)
